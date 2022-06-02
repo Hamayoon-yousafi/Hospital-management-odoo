a7eedbedeebfc763525d4026eaa5a197a54fe32d
hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2022-06-02 10:06:07 +0430

    Hospital Development Commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hosptial Management",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Hamayoon Yousafi",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'mail'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/female_patient_view.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    # will show our module as application
+    'application': True,
+    # we can show our module first in the app list
+    'sequence': -100,
+}
---
+++ om_hospital/models/appointment.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.appointment'
+    _description = 'Hospital Patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    name = fields.Char(string="Name", tracking=True)
+    ref = fields.Char(string="Reference", tracking=True)
+    age = fields.Integer(string="Age", tracking=True)
+    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender", tracking=True)
+    active = fields.Boolean(string="Active", default=True, tracking=True) # Set the default to True so new records are active (unarchived) by defualt.
+
+    
